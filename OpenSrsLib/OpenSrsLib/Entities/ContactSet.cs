1e8ee33a21f676764b09054771e77dc50804ef2e
Elizabeth Young <e.young1980@gmail.com>
2015-03-27 15:17:11 +0000

    Started building entities
    Created project and started to create OpenSrs entities.

---
+++ OpenSrsLib/OpenSrsLib/Entities/ContactSet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSrsLib.Entities
+{
+    public class ContactSet
+    {
+        public Contact Owner { get; set; }
+        public Contact Admin { get; set; }
+        public Contact Billing { get; set; }
+        public Contact Tech { get; set; }
+    }
+}
---
+++ OpenSrsLib/OpenSrsLib/Entities/Domain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSrsLib.Entities
+{
+    public class Domain
+    {
+        public bool AutoRenew { get; set; }
+        public ContactSet ContactSet { get; set; }
+        public bool CustomNameservers { get; set; }
+        public bool CustomTransferNameservers { get; set; }
+        public bool CustomTechContact { get; set; }
+        public string DomainName { get; set; }
+        public bool LockDomain { get; set; }
+        public bool WhoIsPrivacy { get; set; }
+        public string Handle { get; set; }
+    }
+}
